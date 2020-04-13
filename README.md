bcc3078b48376be57d364f27205475cc2c4d34b8
Tezectli <53694320+Tezectli@users.noreply.github.com>
2020-04-13 22:01:33 +0800

    登录注册页实现

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# WA-Admin
-WA商城后台
+# vue-admin2
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Login/index.vue
@@ -0,0 +1,157 @@
+<template>
+  <div id="login">
+    <div class="login-wrap">
+      <ul class="menu-tap">
+        <li v-for="item in menuTab" :key="item.id" :class="{'current': item.current}" @click="toggleMneu(item)">{{item.txt}}</li>
+      </ul>
+      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="login-form" size="medium">
+        
+        <el-form-item prop="username" class="item-form">
+          <label>邮箱啦啦啦啦啦啦啦</label>
+          <el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+        
+        <el-form-item prop="password" class="item-form">
+          <label>密码</label>
+          <el-input type="password" v-model="ruleForm.password" autocomplete="off" minlength="6" maxlength="20"></el-input>
+        </el-form-item>
+        
+        <el-form-item prop="code" class="item-form">
+          <label>验证码</label>
+          <el-row :gutter="10">
+            <el-col :span="15"><el-input v-model.number="ruleForm.code" minlength="6" maxlength="6"></el-input></el-col>
+            <el-col :span="9">
+              <el-button type="success"  class="block">获取验证码</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+        
+        <el-form-item>
+          <el-button type="danger" @click="submitForm('ruleForm')" class="login-btn block">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data(){
+        var validateCode = (rule, value, callback) => {
+        let reg = /^[a-z0-9]{6}$/
+        if (!value) {
+          return callback(new Error('请输入验证码'));
+        } else if(!reg.test(value)){
+          return callback(new Error('验证码格式有误'));
+        } else {
+          callback();
+        }
+      };
+      //下为验证用户名是否为邮箱的方法：validateusername
+      var validateusername = (rule, value, callback) => {
+        let reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+        if (value === '') {
+          callback(new Error('请输入用户名'));
+        } else if(!reg.test(value)){
+          callback(new Error('用户名格式错误'));
+        } else{
+          callback();
+        }
+      };
+      //下为验证密码是否符合格式的方法
+      var validatePassword = (rule, value, callback) => {
+        let reg = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else if (!reg.test(value)) {
+          callback(new Error('密码为6-20位数字+字母!'));
+        } else {
+          callback();
+        }
+      };
+    return{
+      menuTab:[
+        {txt:'登录',current:true},
+        {txt:'注册',current:false}
+      ],
+      // isActive:true
+      //以下为表单数据
+      ruleForm: {
+          username: '',
+          password: '',
+          code: ''
+        },
+        rules: {
+          username: [
+            { validator: validateusername, trigger: 'blur' }
+          ],
+          password: [
+            { validator: validatePassword, trigger: 'blur' }
+          ],
+          code: [
+            { validator: validateCode, trigger: 'blur' }
+          ]
+        }
+    }
+  },
+  methods:{
+    toggleMneu(data){
+      this.menuTab.forEach((elem,index) => {
+        elem.current = false
+      });
+      data.current=true
+    },
+    submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+  }
+};
+</script>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tap {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 80px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #ffffff;
+    border-radius: 2px;
+  }
+  .current {
+    background-color: rgb(86, 168, 179);
+  }
+}
+.login-form {
+  margin-top: 29px;
+  label {
+    display: block;
+    margin-bottom: 3px;
+    font-size: 14px;
+    color: #ffffff;
+  }
+  .item-form { margin-bottom: 13px;}
+  .block {
+    width: 100%;
+    display: block;
+    }
+  .login-btn {
+    margin-top: 19px;
+  }
+}
+</style>
